359d7fbc0dcdabad96855825a30f4537e5bbd05e
Mateus Rodrigues <mateus.pereira.maia94@gmail.com>
2024-01-21 16:12:46 -0300

    feat: created project

---
+++ src/main/java/com/example/clarkechallenge/ClarkeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.clarkechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarkeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClarkeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clarkechallenge/ClarkeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clarkechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClarkeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
